7b910eec9ea4d4e1f7d7764dbe9d0a2d4526fcd8
PrasanthJayaraman <prasanthj@outlook.com>
2017-04-12 22:59:08 +0530

    added socket outside

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var http = require('http');
 var express = require('express');
 var ejs = require('ejs');
 var path = require('path');
-var stream = require('./stream.js');
 var url = require("url");
+
+var logArray = [];
 
 var PORT = 3000;
 
 var app = express();
 
-app.use(stream);
 app.use('/public', express.static(path.join(__dirname + '/public')));
 app.set('views', path.join(__dirname, '/public/views'));
 app.engine('html', require('ejs').renderFile);
@@ -25,16 +25,25 @@ var io = require('socket.io')(server);
 io.on('connection', function (socket) {
   socket.emit('online', { timer: new Date().toISOString(), message: 'Listening to node logger' });
 
-  socket.on('start', function(){
-    socket.emit('online', { timer: new Date().toISOString(), message: 'sample data' });
-  })
-
-  socket.on('stop', function(){
-    console.log('socket stopped');
-    socket.emit('close', { });
-  })
+   socket.on('start', function(){
+     var data;
+     if(logArray && logArray.length >= 1){
+       data = logArray.pop();
+       console.log(data);
+       socket.emit('online', { timer: data.timer, message: data.message });
+     }
+   })
 });
 
-server.listen(PORT, function(){
-  console.log('Node logger is activated, Visit port #'+PORT);
-});
+function info(info){
+  logArray.push({timer : new Date().toISOString(), message : info});
+}
+
+function startLogger(){
+  server.listen(PORT, function(){
+    console.log('Node logger is activated, Visit port #'+PORT);
+  });
+}
+
+exports.startLogger = startLogger;
+exports.info = info;
---
+++ test.js
@@ -0,0 +1,4 @@
+var logger = require('./index.js');
+logger.startLogger();
+logger.info('Hai Prasanth');
+logger.info('How are you da mapla');
